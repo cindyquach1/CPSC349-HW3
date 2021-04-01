1c170d45b81da0b6e6039df594a3442c58de1d15
Cindy Quach <cindyquach96@csu.fullerton.edu>
2021-03-31 22:46:28 -0700

    final commit

---
+++ scripts/payment-form.js
@@ -0,0 +1,10 @@
+// (function (window) {
+//   'use strict';
+//   var PAYMENT_FORM_SELECTOR = '[payment-form-order="form"]';
+//   var App = window.App;
+//   var FormHandler = App.FormHandler;
+//   var formHandler = new FormHandler(PAYMENT_FORM_SELECTOR);
+//   formHandler.addSubmitHandler();
+//   console.log(formHandler);
+
+// })(window);
